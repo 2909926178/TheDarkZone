9a3783db15267bbde64b2790e89946cd26f76663
黄樂成 <80553401+2909926178@users.noreply.github.com>
2023-08-16 14:13:04 +0800

    Update vanillabasedon1.18.30.java

--- vanillabasedon1.18.30.java
+++ vanillabasedon1.18.30.java
@@ -24,51 +24,27 @@
  */
 package javacserver.client;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import javacserver.server.Server;
-import javacserver.shared.PortFileInaccessibleException;
-import javacserver.shared.Protocol;
-import javacserver.shared.Result;
-import javacserver.util.AutoFlushWriter;
-import javacserver.util.Log;
 /**
  * The javacserver client. This is called from the makefiles, and is responsible for passing the command
  * line on to a server instance running javac, starting a new server if needed.
  */
 public class Client {
-    private static final Log.Level LOG_LEVEL = Log.Level.INFO;
-
-    // Wait 2 seconds for response, before giving up on javac server.
-    private static final int CONNECTION_TIMEOUT = 2000;
-    private static final int MAX_CONNECT_ATTEMPTS = 3;
-    private static final int WAIT_BETWEEN_CONNECT_ATTEMPTS = 2000;
-
     private final ClientConfiguration conf;
     public static void main(String... args) {
         Log.setLogForCurrentThread(new Log(
                 new AutoFlushWriter(new OutputStreamWriter(System.out)),
                 new AutoFlushWriter(new OutputStreamWriter(System.err))));
-        Log.setLogLevel(LOG_LEVEL);
-
+        Log.setLogLevel(LOG_LEVEL)
         ClientConfiguration conf = ClientConfiguration.fromCommandLineArguments(args);
         if (conf == null) {
             System.exit(Result.CMDERR.exitCode);
         }
-
         Client client = new Client(conf);
         int exitCode = client.dispatchToServer();
-
         System.exit(exitCode);
     }
 }
