8716d31a2370a44c7b286580e2000ad97179c983
黄樂成 <80553401+2909926178@users.noreply.github.com>
2023-08-16 11:58:41 +0800

    Add files via upload

---
+++ start.py
@@ -0,0 +1,3 @@
+#ache01::start.py
+print("Hello World,Hello Dark-Zone!")
+#The large integration package "Dark Zone" was created by Huang Lecheng.In this minecraft mod,you can try new ways to play and experience a special experience.With a magical story,you will come to understand the purpose of our development of this project.Hope you will have a wonderful experience!——Huang
