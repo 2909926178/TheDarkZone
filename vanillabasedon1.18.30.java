c0a21626969b9e1b3e6910213b98ab7c4b447c8e
黄樂成 <80553401+2909926178@users.noreply.github.com>
2023-08-18 10:03:34 +0800

    Update vanillabasedon1.18.30.java

--- vanillabasedon1.18.30.java
+++ vanillabasedon1.18.30.java
@@ -1,10 +1,8 @@
 package javacserver.client;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-
 public class Client {
     private final ClientConfiguration conf;
     public static void main(String... args) {
@@ -13,9 +11,7 @@ public class Client {
                 new AutoFlushWriter(new OutputStreamWriter(System.err))));
         Log.setLogLevel(LOG_LEVEL)
         ClientConfiguration conf = ClientConfiguration.fromCommandLineArguments(args);
-        if (conf == null) {
-            System.exit(Result.CMDERR.exitCode);
-        }
+        if (conf == null) {System.exit(Result.CMDERR.exitCode);}
         Client client = new Client(conf);
         int exitCode = client.dispatchToServer();
         System.exit(exitCode);
