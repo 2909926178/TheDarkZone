85d7475efae6ede5d7dcabd09738dec82e86e015
黄樂成 <80553401+2909926178@users.noreply.github.com>
2023-08-18 10:02:29 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,8 @@
 #ache01::start.py
 print("Hello World,Hello Dark-Zone!")
 #The large integration package "Dark Zone" was created by Huang Lecheng.In this minecraft mod,you can try new ways to play and experience a special experience.With a magical story,you will come to understand the purpose of our development of this project.Hope you will have a wonderful experience!——Huang
+print("Welcome to Minecraft")
+#Our mod is based on the Chinese version of Minecraft.
+#This mod is completely independent.If you add other mods,they may conflict with this mod.Warning,if this situation was happened.Our suggestion is that you need remove all the mods,keep only this mod.
+#Hope you will have a wonderful playtime.
+print("More information:https://github.com/2909926178/TheDarkZone/edit/main")
